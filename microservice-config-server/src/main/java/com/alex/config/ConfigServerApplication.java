0e4a3c2051ae7003653d01c7b366cc037b5553fa
alex-java <americaf35@Outlook.com>
2018-09-26 23:57:47 +0800

    Spring Cloud Config对称加密示例

--- microservice-config-server/src/main/java/com/alex/config/ConfigServerApplication.java
+++ microservice-config-server/src/main/java/com/alex/config/ConfigServerApplication.java
@@ -4,6 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
+import java.security.Security;
+
 @SpringBootApplication
 @EnableConfigServer
 public class ConfigServerApplication
